3362b4b8f3f4b670b8e8b730330bd842bc24322b
sdrenhao <sdrenhao@aliyun.com>
2017-06-08 16:15:44 +0800

    testgenerate

---
+++ src/test/java/com/mincoo/mbg/test/Generate.java
@@ -0,0 +1,56 @@
+package com.mincoo.mbg.test;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+/**
+ * @author renhao
+ *
+ */
+public class Generate {
+	public void generateMbgConfiguration() throws Exception {
+        /*
+         * Mybatis自带Generator工具生成相应东西
+         */
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        File configFile = new File("./mbg_configuration.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = null;
+        try {
+            config = cp.parseConfiguration(configFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (XMLParserException e) {
+            e.printStackTrace();
+        }
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        try {
+            MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+            myBatisGenerator.generate(null);
+        } catch (InvalidConfigurationException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("生成Mybatis配置成功！");
+    }
+	public static void main(String[] args) throws Exception {
+		new Generate().generateMbgConfiguration();
+	}
+}
